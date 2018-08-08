2b84cbf645dd8f2e73b5c96947cdb0273059c0dd
Verdanmaer <verneradam618@gmail.com>
2018-08-08 22:29:23 +0200

    created HomePage and LoginPage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
+const App = () => <div>
+    <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={LoginPage} />
+</div>
 
 export default App;
